bf50e044f9a1078c47a5345d38299f354cfb9ae6
rtk0c <mail.tianyig@gmail.com>
2024-10-10 11:08:29 -0700

    Update

--- server/mon.go
+++ server/mon.go
@@ -12,6 +12,7 @@ type TmuxSession struct {
 
 type Service struct {
 	Name string
+	WindowIndex int
 	// TODO wtf is this when I wrote it?
 	//ServiceClass string
 }
@@ -30,26 +31,26 @@ func NewTmuxSession(session string) TmuxSession {
 }
 
 // window: an arbitary window name for running the service, uniquely identifying the service
-// commancommand_partsd: see tmux *shell_command* a full shell command for starting the service.
-//          For an abbreviated example: `miniserve -p 1234` results in `/bin/sh -c 'miniserv -p 1234'`,
-//          whereas `miniserve` `-p` `1234` results in running miniserve directly with the arguments.
+// command_parts: see tmux *shell_command* a full shell command for starting the service.
+//     For an abbreviated example: `miniserve -p 1234` results in `/bin/sh -c 'miniserv -p 1234'`,
+//     whereas `miniserve` `-p` `1234` results in running miniserve directly with the arguments.
 func (session *TmuxSession) SpawnService(window string, command_parts []string) {
-	// TODO support multi-process services? like a multi-shard DST cluster
-	/*
-	cmd := exec.Command(TmuxExecutable, "set-option", "-t", session.Name + ":" + window, "synchronize-panes", "on")
-	_, err := cmd.Output()
+	cmd := exec.Command(TmuxExecutable, "new-window", "-t", session.Name + ":", "-n", window, "-P", command_parts...)
+	rest, err := cmd.Output()
 	if err != nil {
 		return nil, err
 	}
-	*/
+	sessionName, rest, _ := fmt.Cut(rest, ":")
+	windowIndex, paneIndex, _ = fmt.Cut(rest, ".")
 
-	cmd := exec.Command(TmuxExecutable, "new-window", "-t", session.Name + ":" + window, command_parts...)
-	_, err := cmd.Output()
+	cmd := exec.Command(TmuxExecutable, "set-option", "-t", session.Name + ":" + windowIndex, "synchronize-panes", "on")
+	err := cmd.Run()
 	if err != nil {
 		return nil, err
 	}
 
 	session.Services = append(session.Services, Service{
 		Name: window,
+		WindowIndex: strconv.Atoi(windowIndex),
 	})
 }
