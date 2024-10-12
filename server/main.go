585ef3b4e67bfa3260c5050f9c25ffcd47adb924
rtk0c <mail.tianyig@gmail.com>
2024-10-12 12:33:25 -0700

    tmux service monitor basic functionality

--- server/main.go
+++ server/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func httpHandler(w http.ResponseWriter, req *http.Request) {
@@ -10,9 +11,26 @@ func httpHandler(w http.ResponseWriter, req *http.Request) {
 }
 
 func main() {
-	http.HandlerFunc("/", httpHandler)
-	http.Listen(":8005", nil)
+	// http.HandleFunc("/", httpHandler)
+	// http.ListenAndServe(":8005", nil)
 
-	ts := NewTmuxSession("Minecraft")
-	ts.SpawnService("stoneblock", [...]string{"/bin/sh"})
+	ts, err := NewTmuxSession("Minecraft")
+	if err != nil {
+		panic(err)
+	}
+
+	stoneblock, err := ts.SpawnService("stoneblock", "/bin/sh")
+	if err != nil {
+		panic(err)
+	}
+	ts.SendKeys(stoneblock, "echo 'Hello, world!'", "Enter")
+	fmt.Printf("stoneblock: %d\n", stoneblock.Status)
+	ts.SendKeys(stoneblock, "exit", "Enter")
+	// Kernel/tmux/the shell is not that fast at handling the exit request. Wait a little bit
+	// This is will happen in the monitor too: periodically per n seconds polling for process status
+	time.Sleep(100 * time.Millisecond)
+	ts.Poll()
+	fmt.Printf("stoneblock: %d\n", stoneblock.Status)
+	ts.Prune()
+	fmt.Println(len(ts.services))
 }
--- server/mon.go
+++ server/mon.go
@@ -3,54 +3,111 @@ package main
 import (
 	"fmt"
 	"os/exec"
+	"strconv"
+	"syscall"
 )
 
 type TmuxSession struct {
-	Name string
-	Services []Service
+	Name     string
+	services []*Service
 }
 
+type ServiceStatus int
+
+const (
+	SS_Stopped ServiceStatus = iota
+	SS_Running
+)
+
 type Service struct {
-	Name string
+	Name        string
 	WindowIndex int
+	Pid         int
+	Status      ServiceStatus
 	// TODO wtf is this when I wrote it?
 	//ServiceClass string
 }
 
 var TmuxExecutable = "/bin/tmux"
 
-func NewTmuxSession(session string) TmuxSession {
+func NewTmuxSession(session string) (*TmuxSession, error) {
 	// Dummy window to keep the session alive
 	cmd := exec.Command(TmuxExecutable, "new-session", "-d", "-s", session, "/bin/sh")
 	_, err := cmd.Output()
 	if err != nil {
-		return nil, fmt.Errorf("Failed to create tmux session: %w", err)
+		return nil, fmt.Errorf("failed to create tmux session: %w", err)
 	}
 
-	return TmuxSession{Name: session}
+	return &TmuxSession{Name: session}, nil
 }
 
 // window: an arbitary window name for running the service, uniquely identifying the service
 // command_parts: see tmux *shell_command* a full shell command for starting the service.
-//     For an abbreviated example: `miniserve -p 1234` results in `/bin/sh -c 'miniserv -p 1234'`,
-//     whereas `miniserve` `-p` `1234` results in running miniserve directly with the arguments.
-func (session *TmuxSession) SpawnService(window string, command_parts []string) {
-	cmd := exec.Command(TmuxExecutable, "new-window", "-t", session.Name + ":", "-n", window, "-P", command_parts...)
-	rest, err := cmd.Output()
+//
+//	For an abbreviated example: `miniserve -p 1234` results in `/bin/sh -c 'miniserv -p 1234'`,
+//	whereas `miniserve` `-p` `1234` results in running miniserve directly with the arguments.
+func (session *TmuxSession) SpawnService(window string, command_parts ...string) (*Service, error) {
+	cmd_arglist := []string{"new-window", "-t", session.Name + ":", "-n", window, "-P", "-F", "#{window_index}:#{pane_pid}"}
+	cmd_arglist = append(cmd_arglist, command_parts...)
+	cmd := exec.Command(TmuxExecutable, cmd_arglist...)
+	windowInfo, err := cmd.Output()
 	if err != nil {
 		return nil, err
 	}
-	sessionName, rest, _ := fmt.Cut(rest, ":")
-	windowIndex, paneIndex, _ = fmt.Cut(rest, ".")
+	var windowIndex, pid int
+	fmt.Sscanf(string(windowInfo), "%d:%d", &windowIndex, &pid)
 
-	cmd := exec.Command(TmuxExecutable, "set-option", "-t", session.Name + ":" + windowIndex, "synchronize-panes", "on")
+	cmd = exec.Command(TmuxExecutable, "set-option", "-t", session.Name+":"+strconv.Itoa(windowIndex), "synchronize-panes", "on")
+	err = cmd.Run()
+	if err != nil {
+		return nil, err
+	}
+
+	serv := &Service{
+		Name:        window,
+		WindowIndex: windowIndex,
+		Pid:         pid,
+		Status:      SS_Running,
+	}
+	session.services = append(session.services, serv)
+	return serv, nil
+}
+
+func (session *TmuxSession) Poll() {
+	for _, serv := range session.services {
+		err := syscall.Kill(serv.Pid, syscall.Signal(0))
+		if err != nil {
+			serv.Pid = 0
+			serv.Status = SS_Stopped
+		}
+	}
+}
+
+func (session *TmuxSession) Prune() {
+	ss := session.services
+	// Technically, the "last unprocessed service" but that's a long name
+	lastAlive := len(ss) - 1
+	i := 0
+	for i <= lastAlive {
+		serv := ss[i]
+		if serv.Status == SS_Stopped {
+			ss[i] = ss[lastAlive]
+			lastAlive--
+		} else {
+			i++
+		}
+	}
+	session.services = ss[:lastAlive+1]
+}
+
+func (session *TmuxSession) SendKeys(serv *Service, keys ...string) error {
+	cmd_arglist := []string{"send-keys", "-t", session.Name + ":" + serv.Name}
+	cmd_arglist = append(cmd_arglist, keys...)
+	cmd := exec.Command(TmuxExecutable, cmd_arglist...)
 	err := cmd.Run()
 	if err != nil {
-		return nil, err
+		return err
 	}
 
-	session.Services = append(session.Services, Service{
-		Name: window,
-		WindowIndex: strconv.Atoi(windowIndex),
-	})
+	return nil
 }
