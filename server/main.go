46092127b0fc8c0e809b16321bd0e7f4c768236f
rtk0c <mail.tianyig@gmail.com>
2024-10-12 21:54:35 -0700

    API for starting/stopping units

--- server/main.go
+++ server/main.go
@@ -8,15 +8,34 @@ import (
 	"time"
 )
 
+var ts *TmuxSession
 var registar *UnitRegistrar
 var regLock sync.RWMutex
 
 func httpUnit(w http.ResponseWriter, u *Unit) {
 	fmt.Fprintf(w, `
 <div id="unit.%[1]s" class="unit">
-<span class="unit-name">%[1]s</span>
-</div>
+<p class="unit-name">%[1]s</p>
 `, u.Name)
+
+	switch u.Status {
+	case US_Running:
+		fmt.Fprint(w, `<span class="marker marker-running">Running</span>`)
+		fmt.Fprintf(w, `
+<form method="post" action="/api/stop-unit">
+<input type="hidden" name="unit" value="%s">
+<input type="submit" value="Stop">
+</form>`, u.Name)
+	case US_Stopped:
+		fmt.Fprint(w, `<span class="marker marker-stopped">Stopped</span>`)
+		fmt.Fprintf(w, `
+<form method="post" action="/api/start-unit">
+<input type="hidden" name="unit" value="%s">
+<input type="submit" value="Start">
+</form>`, u.Name)
+	}
+
+	fmt.Fprintln(w, "</div>")
 }
 
 func httpDisplayRegistrar(w http.ResponseWriter) {
@@ -47,11 +66,51 @@ func httpHandler(w http.ResponseWriter, req *http.Request) {
 </html>`)
 }
 
+func apiStartUnit(w http.ResponseWriter, req *http.Request) {
+	if req.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	req.ParseForm()
+	unitName := req.Form.Get("unit")
+	unit := registar.unitsLut[unitName]
+	registar.StopUnit(unit)
+
+	fmt.Printf("got /api/start-unit for unit=%s\n", unitName)
+
+	http.Redirect(w, req, "/", http.StatusFound)
+}
+
+func apiStopUnit(w http.ResponseWriter, req *http.Request) {
+	if req.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	req.ParseForm()
+	unitName := req.Form.Get("unit")
+	unit := registar.unitsLut[unitName]
+	registar.StartUnit(unit)
+
+	fmt.Printf("got /api/stop-unit for unit=%s\n", unitName)
+
+	http.Redirect(w, req, "/", http.StatusFound)
+}
+
 func main() {
-	ts, err := NewTmuxSession("Minecraft")
+	var err error
+
+	ts, err = NewTmuxSession("Minecraft")
 	if err != nil {
 		panic(err)
 	}
+
+	registar, err = NewUnitRegistrar(ts)
+	if err != nil {
+		panic(err)
+	}
+
 	tsPollTimer := time.NewTicker(5 * time.Second)
 	tsPollStop := make(chan bool)
 	go func() {
@@ -60,22 +119,22 @@ func main() {
 			case <-tsPollTimer.C:
 				ts.Poll()
 				ts.Prune()
+				regLock.Lock()
+				registar.MapFromTmux()
+				regLock.Unlock()
 			case <-tsPollStop:
 				return
 			}
 		}
 	}()
 
-	registar, err = NewUnitRegistrar(ts)
-	if err != nil {
-		panic(err)
-	}
-
 	staticFilesDir := flag.String("static-files", ".", "Path to the directory holding static files")
 	flag.Parse()
 	fmt.Println(*staticFilesDir)
 
 	http.HandleFunc("/", httpHandler)
+	http.HandleFunc("/api/start-unit", apiStartUnit)
+	http.HandleFunc("/api/stop-unit", apiStopUnit)
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir(*staticFilesDir))))
 	http.ListenAndServe(":8005", nil)
 }
--- server/unit.go
+++ server/unit.go
@@ -4,22 +4,24 @@ type UnitStatus int
 
 const (
 	US_Stopped UnitStatus = iota
-	US_StoppedOnError
 	US_Running
 )
 
 type Unit struct {
+	procGroup    *TmuxProcGroup
 	Name         string
-	startCommand string
-	stopCommand  string
+	startCommand []string
+	stopCommand  []string
 	Status       UnitStatus
 }
 
 type UnitRegistrar struct {
 	tmux *TmuxSession
-	// List of units, lexigraphically sorted by [Unit.Name]
+	// List of units, lexigraphically sorted by [Unit.Name].
+	// Immutable after load.
 	units []*Unit
-	// Lookup table from [Unit.Name] to the [Unit] itself
+	// Lookup table from [Unit.Name] to the [Unit] itself.
+	// Immutable after load.
 	unitsLut map[string]*Unit
 }
 
@@ -32,12 +34,47 @@ func NewUnitRegistrar(tmux *TmuxSession) (*UnitRegistrar, error) {
 	}
 
 	var u *Unit
-	u = &Unit{Name: "Baking", startCommand: "./run", stopCommand: "stop"}
+	u = &Unit{Name: "Baking", startCommand: []string{"./run"}, stopCommand: []string{"stop", "Enter"}}
 	r.units = append(r.units, u)
 	r.unitsLut["Baking"] = u
-	u = &Unit{Name: "Stoneblock 3", startCommand: "./run", stopCommand: "stop"}
+	u = &Unit{Name: "Stoneblock 3", startCommand: []string{"./run"}, stopCommand: []string{"stop", "Enter"}}
 	r.unitsLut["Stoneblock 3"] = u
 	r.units = append(r.units, u)
 
 	return r, nil
+}
+
+func (reg *UnitRegistrar) StartUnit(unit *Unit) {
+	if unit.Status != US_Stopped {
+		return
+	}
+
+	reg.tmux.SpawnProcesses(unit.Name, unit.startCommand...)
+}
+
+func updateUnitFromTmux(unit *Unit) {
+	if unit.procGroup.Dead {
+		unit.Status = US_Stopped
+		unit.procGroup = nil
+	}
+}
+
+func (reg *UnitRegistrar) StopUnit(unit *Unit) {
+	if unit.Status != US_Running {
+		return
+	}
+
+	reg.tmux.SendKeys(unit.procGroup, unit.stopCommand...)
+	updateUnitFromTmux(unit)
+}
+
+func (reg *UnitRegistrar) MapFromTmux() {
+	for _, unit := range reg.units {
+		if unit.procGroup == nil {
+			continue
+		}
+
+		updateUnitFromTmux(unit)
+	}
+	// TODO map from "wild" tmux windows/process groups
 }
