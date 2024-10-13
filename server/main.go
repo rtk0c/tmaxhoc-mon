456655ba2587194729e6c71ff482739161197bf5
rtk0c <mail.tianyig@gmail.com>
2024-10-12 20:54:22 -0700

    Unit handling draft

--- server/main.go
+++ server/main.go
@@ -1,36 +1,80 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net/http"
+	"sync"
 	"time"
 )
 
+var registar *UnitRegistrar
+var regLock sync.RWMutex
+
+func httpUnit(w http.ResponseWriter, u *Unit) {
+	fmt.Fprintf(w, `
+<div id="unit.%[1]s" class="unit">
+<span class="unit-name">%[1]s</span>
+</div>
+`, u.Name)
+}
+
+func httpDisplayRegistrar(w http.ResponseWriter) {
+	regLock.RLock()
+
+	fmt.Fprintln(w, `<div class="unit-container">`)
+	for _, unit := range registar.units {
+		httpUnit(w, unit)
+	}
+	fmt.Fprintln(w, `</div>`)
+
+	regLock.RUnlock()
+}
+
 func httpHandler(w http.ResponseWriter, req *http.Request) {
-	// TODO
+	fmt.Fprintln(w, `
+<!DOCTYPE html><html>
+<head>
+<title>tmaxhoc</title>
+</head>
+<body>`)
+
+	httpDisplayRegistrar(w)
+
+	fmt.Fprintln(w, `
+</body>
+</html>`)
 }
 
 func main() {
-	// http.HandleFunc("/", httpHandler)
-	// http.ListenAndServe(":8005", nil)
-
 	ts, err := NewTmuxSession("Minecraft")
 	if err != nil {
 		panic(err)
 	}
+	tsPollTimer := time.NewTicker(5 * time.Second)
+	tsPollStop := make(chan bool)
+	go func() {
+		for {
+			select {
+			case <-tsPollTimer.C:
+				ts.Poll()
+				ts.Prune()
+			case <-tsPollStop:
+				return
+			}
+		}
+	}()
 
-	stoneblock, err := ts.SpawnService("stoneblock", "/bin/sh")
+	registar, err = NewUnitRegistrar(ts)
 	if err != nil {
 		panic(err)
 	}
-	ts.SendKeys(stoneblock, "echo 'Hello, world!'", "Enter")
-	fmt.Printf("stoneblock: %d\n", stoneblock.Status)
-	ts.SendKeys(stoneblock, "exit", "Enter")
-	// Kernel/tmux/the shell is not that fast at handling the exit request. Wait a little bit
-	// This is will happen in the monitor too: periodically per n seconds polling for process status
-	time.Sleep(100 * time.Millisecond)
-	ts.Poll()
-	fmt.Printf("stoneblock: %d\n", stoneblock.Status)
-	ts.Prune()
-	fmt.Println(len(ts.services))
+
+	staticFilesDir := flag.String("static-files", ".", "Path to the directory holding static files")
+	flag.Parse()
+	fmt.Println(*staticFilesDir)
+
+	http.HandleFunc("/", httpHandler)
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir(*staticFilesDir))))
+	http.ListenAndServe(":8005", nil)
 }
---
+++ server/unit.go
@@ -0,0 +1,34 @@
+package main
+
+type Unit struct {
+	Name         string
+	startCommand string
+	stopCommand  string
+}
+
+type UnitRegistrar struct {
+	tmux     *TmuxSession
+	// List of units, lexigraphically sorted by [Unit.Name]
+	units    []*Unit
+	// Lookup table from [Unit.Name] to the [Unit] itself
+	unitsLut map[string]*Unit
+}
+
+// TODO read config file for units
+func NewUnitRegistrar(tmux *TmuxSession) (*UnitRegistrar, error) {
+	r := &UnitRegistrar{
+		tmux:     tmux,
+		units:    make([]*Unit, 0),
+		unitsLut: make(map[string]*Unit),
+	}
+
+	var u *Unit
+	u = &Unit{Name: "Baking", startCommand: "./run", stopCommand: "stop"}
+	r.units = append(r.units, u)
+	r.unitsLut["Baking"] = u
+	u = &Unit{Name: "Stoneblock 3", startCommand: "./run", stopCommand: "stop"}
+	r.unitsLut["Stoneblock 3"] = u
+	r.units = append(r.units, u)
+
+	return r, nil
+}
