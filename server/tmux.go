250f2e118a23a9b4498017fca1f0eaf9670511a3
rtk0c <mail.tianyig@gmail.com>
2024-10-14 17:05:24 -0700

    Unify variable names tmux.go

--- server/tmux.go
+++ server/tmux.go
@@ -42,9 +42,9 @@ func NewTmuxSession(session string) (*TmuxSession, error) {
 	return &TmuxSession{Name: session}, nil
 }
 
-func (ss *TmuxSession) insertProcGroup(procGroup *TmuxProcGroup) {
-	ss.procGroups = append(ss.procGroups, procGroup)
-	ss.lut[procGroup.Pid] = procGroup
+func (ts *TmuxSession) insertProcGroup(procGroup *TmuxProcGroup) {
+	ts.procGroups = append(ts.procGroups, procGroup)
+	ts.lut[procGroup.Pid] = procGroup
 }
 
 // windowName is an arbitary tmux window name for running the processes in.
@@ -54,10 +54,10 @@ func (ss *TmuxSession) insertProcGroup(procGroup *TmuxProcGroup) {
 // whereas `miniserve` `-p` `1234` results in running miniserve directly with the arguments.
 //
 // TODO multiple processes
-func (session *TmuxSession) SpawnProcesses(windowName string, commandParts ...string) (*TmuxProcGroup, error) {
-	cmd_arglist := []string{"new-window", "-t", session.Name + ":", "-n", windowName, "-P", "-F", "#{window_index}:#{pane_pid}"}
-	cmd_arglist = append(cmd_arglist, commandParts...)
-	cmd := exec.Command(TmuxExecutable, cmd_arglist...)
+func (ts *TmuxSession) SpawnProcesses(windowName string, commandParts ...string) (*TmuxProcGroup, error) {
+	cmdArglist := []string{"new-window", "-t", ts.Name + ":", "-n", windowName, "-P", "-F", "#{window_index}:#{pane_pid}"}
+	cmdArglist = append(cmdArglist, commandParts...)
+	cmd := exec.Command(TmuxExecutable, cmdArglist...)
 	windowInfo, err := cmd.Output()
 	if err != nil {
 		return nil, err
@@ -65,7 +65,7 @@ func (session *TmuxSession) SpawnProcesses(windowName string, commandParts ...st
 	var windowIndex, pid int
 	fmt.Sscanf(string(windowInfo), "%d:%d", &windowIndex, &pid)
 
-	cmd = exec.Command(TmuxExecutable, "set-option", "-t", session.Name+":"+strconv.Itoa(windowIndex), "synchronize-panes", "on")
+	cmd = exec.Command(TmuxExecutable, "set-option", "-t", ts.Name+":"+strconv.Itoa(windowIndex), "synchronize-panes", "on")
 	err = cmd.Run()
 	if err != nil {
 		return nil, err
@@ -76,12 +76,12 @@ func (session *TmuxSession) SpawnProcesses(windowName string, commandParts ...st
 		WindowIndex: windowIndex,
 		Pid:         pid,
 	}
-	session.insertProcGroup(procGroup)
+	ts.insertProcGroup(procGroup)
 	return procGroup, nil
 }
 
-func (session *TmuxSession) Poll() error {
-	for _, serv := range session.procGroups {
+func (ts *TmuxSession) Poll() error {
+	for _, serv := range ts.procGroups {
 		err := syscall.Kill(serv.Pid, syscall.Signal(0))
 		if err != nil {
 			serv.Pid = 0
@@ -89,7 +89,7 @@ func (session *TmuxSession) Poll() error {
 		}
 	}
 
-	cmd := exec.Command(TmuxExecutable, "list-panes", "-s", "-t", session.Name+":", "-F", "#{window_index}$#{pane_pid}$#{window_name}")
+	cmd := exec.Command(TmuxExecutable, "list-panes", "-s", "-t", ts.Name+":", "-F", "#{window_index}$#{pane_pid}$#{window_name}")
 	panes, err := cmd.Output()
 	if err != nil {
 		return err
@@ -107,7 +107,7 @@ func (session *TmuxSession) Poll() error {
 		}
 		windowName := parts[2]
 
-		procGroup := session.lut[windowIndex]
+		procGroup := ts.lut[windowIndex]
 		if procGroup == nil {
 			procGroup = &TmuxProcGroup{
 				Name:        windowName,
@@ -115,35 +115,34 @@ func (session *TmuxSession) Poll() error {
 				Pid:         pid,
 				Orphan:      true,
 			}
-			session.insertProcGroup(procGroup)
+			ts.insertProcGroup(procGroup)
 		}
 	}
 
 	return nil
 }
 
-func (session *TmuxSession) Prune() {
-	ss := session.procGroups
+func (ts *TmuxSession) Prune() {
+	procGroups := ts.procGroups
 	// Technically, the "last unprocessed service" but that's a long name
-	lastAlive := len(ss) - 1
+	lastAlive := len(procGroups) - 1
 	i := 0
 	for i <= lastAlive {
-		serv := ss[i]
-		if serv.Dead {
-			delete(session.lut, serv.Pid)
-			ss[i] = ss[lastAlive]
+		if procGroups[i].Dead {
+			delete(ts.lut, procGroups[i].Pid)
+			procGroups[i] = procGroups[lastAlive]
 			lastAlive--
 		} else {
 			i++
 		}
 	}
-	session.procGroups = ss[:lastAlive+1]
+	ts.procGroups = procGroups[:lastAlive+1]
 }
 
 func (session *TmuxSession) SendKeys(serv *TmuxProcGroup, keys ...string) error {
-	cmd_arglist := []string{"send-keys", "-t", session.Name + ":" + serv.Name}
-	cmd_arglist = append(cmd_arglist, keys...)
-	cmd := exec.Command(TmuxExecutable, cmd_arglist...)
+	cmdArglist := []string{"send-keys", "-t", session.Name + ":" + serv.Name}
+	cmdArglist = append(cmdArglist, keys...)
+	cmd := exec.Command(TmuxExecutable, cmdArglist...)
 	err := cmd.Run()
 	if err != nil {
 		return err
